df35f33276a9098563f6110fd71d17d862064a26
ShahŊooɽ <109735777+TECH-SHANI@users.noreply.github.com>
2023-07-22 13:59:20 +0500

    Create FILE.py

---
+++ FILE.py
@@ -0,0 +1,10 @@
+import os, sys, platform,time
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    os.system('git pull')
+    import FILE    
+elif bit == '32bit':
+    os.system('clear')
+    os.system('git pull')
+    import FILE
